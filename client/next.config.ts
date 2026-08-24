76be01913a1c3b424de6969f39095515591fe91a
Mausam Baduwal <baduwalmausam77@gmail.com>
2026-08-24 14:32:08 +0545

    fix external image configuration

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
  images: {
     remotePatterns: [
+       {
+        protocol: "https",
+        hostname: "authjs.dev",
+      },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
